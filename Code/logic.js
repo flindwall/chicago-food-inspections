24157d743c56dfac2859ec76ab44669c0885bcdf
Genevieve Nimeth <genevieve.n.data@gmail.com>
2023-02-17 20:19:22 -0600

    Upload updated javascript

--- Code/logic.js
+++ Code/logic.js
@@ -1,59 +1,83 @@
-function popUpMsg(feature, layer) {
-    layer.bindPopup("<h3>" + feature.properties.aka_name + "</h3><hr><p>" +
-      new Date(feature.properties.inspection_date) + "</p><p>Address: " + feature.properties.address + "</p>");
-  };
-
- // Define streetmap and darkmap layers
- let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1
-  });
+// Define streetmap and darkmap layers
+let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1
+});
 
 // Define a baseMaps object to hold our base layers
 let baseMaps = {
-    "Street Map": streetmap
-    };  
+  "Street Map": streetmap
+};  
 
-// Create our map, giving it the streetmap and earthquakes layers to display on load
+// Create our map, giving it the streetmap and others layers to display on load
 let myMap = L.map("map", {
-    center: [ 37.09, -95.71 ],
-    zoom: 3,
-    layers: [streetmap]     //default selected layer
-    });
+  center: [ 41.90, -87.73 ],
+  zoom: 11,
+  layers: [] // no layers added initially
+});
 
 // Add streetmap tile to map
 streetmap.addTo(myMap);
 
-
-// create layer; will attach data later on
-let riskscore = new L.LayerGroup();
-
-// Create overlay object to hold our overlay layer
+// Create overlay objects to hold our overlay layers
 let overlayMaps = {
-	Riskscore: riskscore
+  "Risk Score 1 - Pass": new L.LayerGroup(),
+  "Risk Score 2 - Pass": new L.LayerGroup(),
+  "Risk Score 3 - Pass": new L.LayerGroup(),
+  "Risk Score 1 - Fail": new L.LayerGroup(),
+  "Risk Score 2 - Fail": new L.LayerGroup(),
+  "Risk Score 3 - Fail": new L.LayerGroup()
 }
 
 // Create a layer control
 // Pass in our baseMaps and overlayMaps
-
 L.control.layers(baseMaps, overlayMaps, {
-	collapsed: false
-  }).addTo(myMap);
+  collapsed: false
+}).addTo(myMap);
 
+// Create a custom legend title
+let legendTitle = L.control({ position: 'topright' });
+legendTitle.onAdd = function(map) {
+  let div = L.DomUtil.create('div', 'legend-title');
+  div.innerHTML = 'Restaurants in Chicago 2022-current';
+  return div;
+};
+legendTitle.addTo(myMap);
 
-const url = "https://data.cityofchicago.org/resource/4ijn-s7e5.geojson?$limit=250";
+// Load the data and add it to the corresponding layer group when the user selects the layer
+const url = "https://data.cityofchicago.org/resource/4ijn-s7e5.geojson?$limit=10000";
 
-// Perform a GET request to the query URL
 d3.json(url).then(function(data) {  
-
-	L.geoJSON(data, {
-		onEachFeature: popUpMsg,
-		pointToLayer: function(feature, latlng) {
-			return L.marker(latlng); // create a new marker layer for each point
-		}
-	}).addTo(myMap);	
-	
-	// riskscore.addTo(myMap);
+  data.features.forEach(function(feature) {
+    if (feature.properties.facility_type === "Restaurant" && 
+        feature.properties.city === "CHICAGO" && 
+        feature.properties.inspection_date > "2022-01-01T00:00:00.000") {
+      
+      let layer;
+      
+      if (feature.properties.risk === "Risk 1 (High)" && feature.properties.results === "Pass") {
+        layer = overlayMaps["Risk Score 1 - Pass"];
+      } else if (feature.properties.risk === "Risk 2 (Medium)" && feature.properties.results === "Pass") {
+        layer = overlayMaps["Risk Score 2 - Pass"];
+      } else if (feature.properties.risk === "Risk 3 (Low)" && feature.properties.results === "Pass") {
+        layer = overlayMaps["Risk Score 3 - Pass"];
+      } else if (feature.properties.risk === "Risk 1 (High)" && feature.properties.results === "Fail") {
+        layer = overlayMaps["Risk Score 1 - Fail"];
+      } else if (feature.properties.risk === "Risk 2 (Medium)" && feature.properties.results === "Fail") {
+        layer = overlayMaps["Risk Score 2 - Fail"];  
+      } else if (feature.properties.risk === "Risk 3 (Low)" && feature.properties.results === "Fail") {
+        layer = overlayMaps["Risk Score 3 - Fail"];
+      }  
+      
+      if (layer) {
+        let marker = L.marker([feature.geometry.coordinates[1], feature.geometry.coordinates[0]]);
+        marker.bindPopup("<h3>" + feature.properties.aka_name + "</h3><hr><p>Address: " + feature.properties.address + "</p><p>Risk Score: " + feature.properties.risk + "</p><p>" + feature.properties.results + "</p>");
+        layer.addLayer(marker);
+      }
+    }
+  });
 });
+
+
